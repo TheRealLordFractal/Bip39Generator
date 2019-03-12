add604fc7944d9d5bb84b29f74d9b1b98269ca50
TheRealLordFractal <40230109+TheRealLordFractal@users.noreply.github.com>
2019-03-12 14:57:24 -0700

    Added support for Autosave to 12words.txt,
    Fixed misc programming errors, performance increases

--- generate12words.py
+++ generate12words.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-code_phrase_geneator.py
 
-#Created by Scott Roberts.
-#Copyright (c) 2011 TogaFoamParty Studios. All rights reserved.
 
-# Updated to use Bip39 by TheRealLordFractal 2019
+#  Bip39 by TheRealLordFractal 2019
 
 """
 
@@ -14,16 +11,18 @@ import sys
 import getopt
 import random
 
+#sys.stdout = open('output.txt','wt')
 
 help_message = '''
-Code Phrase Generator
+Bip39 12 Word Random Generator By TheRealLordFractal
+Output File Saved as 12words.txt
+Command Exntensions:
 -n <x> or --number <x>: Number of sample code phrases given. (Default is 5)
--p <prefix> or --prefix <prefix>: Uses a prefix word along with a random codeword instead of two codewords.
 -w <file> or --wordlist <file>: Uses another wordlist to generate code phrases from.
 '''
 
 CODEWORDS = open('wordlist.txt', 'r').readlines()
-
+outputtxt = open('12words.txt', 'w')
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -32,24 +31,24 @@ class Usage(Exception):
 def generate(prefix=False, number=5):
     while number > 0:        
         if prefix == 'TRUE':
-            word1 = PREFIXES[int(random.uniform(0,len(PREFIXES)))]
+           print ("Not Supported.")
+           # word1 = PREFIXES[int(random.uniform(0,len(PREFIXES)))]
         elif prefix:
             word1 = prefix
         else:
             word1 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-        
-        word2 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word3 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word4 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word5 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word6 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word7 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word8 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word9 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word10 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word11 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word12 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-        print " %s %s %s %s %s %s %s %s %s %s %s %s " % (word1.rstrip(), word2.rstrip(), word3.rstrip(), word4.rstrip(), word5.rstrip(), word6.rstrip(), word7.rstrip(), word8.rstrip(), word9.rstrip(), word10.rstrip(), word11.rstrip(), word12.rstrip())
+            word2 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word3 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word4 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word5 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word6 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word7 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word8 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word9 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word10 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word11 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word12 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+        print >> outputtxt, " %s %s %s %s %s %s %s %s %s %s %s %s " % (word1.rstrip(), word2.rstrip(), word3.rstrip(), word4.rstrip(), word5.rstrip(), word6.rstrip(), word7.rstrip(), word8.rstrip(), word9.rstrip(), word10.rstrip(), word11.rstrip(), word12.rstrip())
         
         number -= 1
     
@@ -76,9 +75,9 @@ def main(argv=None):
             if option in ("-n", "--number"):
                 number = int(value)
             if option in ("-w", "--wordlist"):
-            	global CODEWORDS
-            	print "Importing: %s" % value
-            	CODEWORDS = open(value, 'r').readlines()
+                global CODEWORDS
+                print "Importing: %s" % value
+                CODEWORDS = open(value, 'r').readlines()
             if option in ("-p", "--prefixe"):
                 
                 print value
