fe8dc3cea113425c7c340b91147e8d5f478351e7
TheRealLordFractal <40230109+TheRealLordFractal@users.noreply.github.com>
2019-03-12 16:33:31 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -17,25 +17,33 @@ Just running the script from the commandline will randomly generate 3, 6, 12 BIP
 
 Running the script is simple
 
-EXAMPLE:
+USAGE EXAMPLES:
 
 
 Make it super simple I have encluded the BIP39.txt files for the different languages,  all you need to do is choose your target.  I will update and work on this more.. However right now all you need to do is for EXAMPLE
 english, copy the english.txt to wordlist.txt      In unix its cp english.txt wordlist.txt    This sets up the wordlist for your target or you can use the -w flash for wordlist file
 
-./generate12words.py -n 5000000 >> 12words.txt
+./generate12words.py -n 5000000  
 
-In this example the script generates 12 random words per line of a text file, the -n specifys the # of lines you wish to make your txt file, and the >> 12words.txt is the output file for writing
+In this example the script generates 12 random words per line of a text file, the -n specifys the # of lines you wish to make your txt file, and then save them to 12words.txt as output file
+*NOTE* This can create VERY LARGE .txt files depending how many <-n> you pass to the script
 
+-
+
+This example Does not create any massive txt file and directs output directly to brainflayer, this is the most effect and fastest way to start checking BIP39 phrases
+
+./brain12words.py -n 9000000000000000000 | ../brainflayer/brainflayer -v -m tablefile.tab -o foundkeys.txt -b testfile.blm
+
+
+SAMPLE OUTPUT:
+
+rate:  270111.98 p/s found:     0/786432     elapsed:   11.218 s
 
 ## Operators
 * -n <number of lines you wish to create>
 
 	* Generates <x> code phrases. Without selecting this option the default is 5.
 
-* -p <prefix> or --prefix <prefix>
-
-	* Generates phrases using <prefix> as the first word. Useful for related groups of code phrases.
 
 * -w <file> or --wordlist <file>
 
---
+++ brain12words.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+
+#  Bip39 by TheRealLordFractal 2019
+
+"""
+
+import sys
+import getopt
+import random
+
+#sys.stdout = open('output.txt','wt')
+
+help_message = '''
+Bip39 12 Word Random Generator By TheRealLordFractal
+Output File Saved as 12words.txt
+Command Exntensions:
+-n <x> or --number <x>: Number of sample code phrases given. (Default is 5)
+-w <file> or --wordlist <file>: Uses another wordlist to generate code phrases from.
+'''
+
+CODEWORDS = open('wordlist.txt', 'r').readlines()
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def generate(prefix=False, number=5):
+    while number > 0:        
+        if prefix == 'TRUE':
+           print ("Not Supported.")
+           # word1 = PREFIXES[int(random.uniform(0,len(PREFIXES)))]
+        elif prefix:
+            word1 = prefix
+        else:
+            word1 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word2 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word3 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word4 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word5 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word6 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word7 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word8 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word9 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word10 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word11 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word12 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+        print " %s %s %s %s %s %s %s %s %s %s %s %s " % (word1.rstrip(), word2.rstrip(), word3.rstrip(), word4.rstrip(), word5.rstrip(), word6.rstrip(), word7.rstrip(), word8.rstrip(), word9.rstrip(), word10.rstrip(), word11.rstrip(), word12.rstrip())
+        
+        number -= 1
+    
+
+def main(argv=None):
+    
+    number = 5
+    prefix = False
+    
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hn:p:i:vw:", ["help", "number=", "prefix=", "wordlist="])
+        except getopt.error, msg:
+            raise Usage(msg)
+    
+        # option processing
+        for option, value in opts:
+            if option == "-v":
+                verbose = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+            if option in ("-n", "--number"):
+                number = int(value)
+            if option in ("-w", "--wordlist"):
+                global CODEWORDS
+                print "Importing: %s" % value
+                CODEWORDS = open(value, 'r').readlines()
+            if option in ("-p", "--prefixe"):
+                
+                print value
+                
+                if (value):
+                    prefix = value
+                else:
+                    prefix = 'TRUE'
+    
+        generate(prefix, number)
+    
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
