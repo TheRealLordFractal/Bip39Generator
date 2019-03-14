6ebab094800197d502ffdeb5559bc233ac169be4
TheRealLordFractal <40230109+TheRealLordFractal@users.noreply.github.com>
2019-03-14 12:50:58 -0700

    Fixed generate6words to comply with same format
    Updated generate6words.py to output to 6words.txt as per ticket 2

--- generate6words.py
+++ generate6words.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-code_phrase_geneator.py
 
-#Created by Scott Roberts.
-#Copyright (c) 2011 TogaFoamParty Studios. All rights reserved.
 
-# Updated to use Bip39 by TheRealLordFractal 2019
+#  Bip39 by TheRealLordFractal 2019
 
 """
 
@@ -14,16 +11,18 @@ import sys
 import getopt
 import random
 
+#sys.stdout = open('output.txt','wt')
 
 help_message = '''
-Code Phrase Generator
+Bip39 6 Word Random Generator By TheRealLordFractal
+Output File Saved as 12words.txt
+Command Exntensions:
 -n <x> or --number <x>: Number of sample code phrases given. (Default is 5)
--p <prefix> or --prefix <prefix>: Uses a prefix word along with a random codeword instead of two codewords.
 -w <file> or --wordlist <file>: Uses another wordlist to generate code phrases from.
 '''
 
 CODEWORDS = open('wordlist.txt', 'r').readlines()
-
+outputtxt = open('6words.txt', 'w')
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -32,18 +31,18 @@ class Usage(Exception):
 def generate(prefix=False, number=5):
     while number > 0:        
         if prefix == 'TRUE':
-            word1 = PREFIXES[int(random.uniform(0,len(PREFIXES)))]
+           print ("Not Supported.")
+           # word1 = PREFIXES[int(random.uniform(0,len(PREFIXES)))]
         elif prefix:
             word1 = prefix
         else:
             word1 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-        
-        word2 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word3 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word4 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word5 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-	word6 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
-        print " %s %s %s %s %s %s" % (word1.rstrip(), word2.rstrip(), word3.rstrip(), word4.rstrip(), word5.rstrip(), word6.rstrip())
+            word2 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word3 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word4 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word5 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+            word6 = CODEWORDS[int(random.uniform(0,len(CODEWORDS)))]
+        print >> outputtxt, " %s %s %s %s %s %s " % (word1.rstrip(), word2.rstrip(), word3.rstrip(), word4.rstrip(), word5.rstrip(), word6.rstrip())
         
         number -= 1
     
@@ -70,9 +69,9 @@ def main(argv=None):
             if option in ("-n", "--number"):
                 number = int(value)
             if option in ("-w", "--wordlist"):
-            	global CODEWORDS
-            	print "Importing: %s" % value
-            	CODEWORDS = open(value, 'r').readlines()
+                global CODEWORDS
+                print "Importing: %s" % value
+                CODEWORDS = open(value, 'r').readlines()
             if option in ("-p", "--prefixe"):
                 
                 print value
